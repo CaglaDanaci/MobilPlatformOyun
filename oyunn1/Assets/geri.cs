6032da1e64e9f792c3d1f6723624be7e4182b8d0
CaglaDanaci <32449086+CaglaDanaci@users.noreply.github.com>
2018-05-11 13:54:17 +0300

    oyun

---
+++ oyunn1/Assets/geri.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class geri : MonoBehaviour {
+
+	public void bolum_geri(int bolum_sistemi)
+	{
+		Application.LoadLevel (bolum_sistemi);
+	}
+}
